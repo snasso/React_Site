c13ccc9c59e7a89938f3eb3641d1f95ba75fe326
Sandro Nasso <sandro.ordnas@live.ca>
2018-02-14 15:06:12 -0500

    updated Articles Section

--- src/components/Article.js
+++ src/components/Article.js
@@ -8,7 +8,7 @@ class Article extends Component {
             <div className="col s12 m6 article">
                 <img src={this.props.image} />
                 <p className="text">
-                    <span className="textTitle">What is Lorem Ipsum?</span>
+                    <span className="textTitle">What is Lorem Ipsum?</span><br />
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
                     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
                     scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
